d81f75eec38a5b9be42f65408c52cbf0b4533e26
Miguel Martins Duarte <miguel.duarte20@hotmail.com>
2016-02-20 20:23:22 +0000

    Capital letters UI helper

--- client/templates/application/newspaper/newspaper.js
+++ client/templates/application/newspaper/newspaper.js
@@ -88,3 +88,10 @@ Template.newsArticle.helpers({
     return this.img;
   }
 });
+
+UI.registerHelper('capital', function(s, options) {
+  s = s.split(' ');
+  for(i=0; i<s.length; i++)
+    s[i] = s[i].charAt(0).toUpperCase() + s[i].slice(1);
+  return s.join(' ');
+});
